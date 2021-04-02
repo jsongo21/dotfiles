33202bd9993cd76a8d8f4aa17c1720ca0cec6aca
Jason Ngo <jason_ngo1@hotmail.com>
2021-04-02 11:03:31 +0000

    added bash_profile

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create symlinks for dotfiles to $HOME
-rc_files=$(ls .*rc .Xresources)
+rc_files=$(ls .*rc .bash_profile .Xresources)
 
 for i in $rc_files; do
     ln -svf "$(pwd)/$i" ~/$i
