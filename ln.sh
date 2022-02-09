94248df2e43a00a00bfce623c782c0c11ac780c4
Jason Ngo <1.ngo.jason@gmail.com>
2022-02-09 15:49:47 +1100

    fixed file name spelling erorr

--- ln.sh
+++ ln.sh
@@ -1,4 +1,4 @@
 ln -sf "${HOME}"/dotfiles/.zshrc "${HOME}"/.zshrc
 ln -sf "${HOME}"/dotfiles/.tmux.conf "${HOME}"/.tmux.conf
 ln -sf "${HOME}"/dotfiles/.vimrc "${HOME}"/.vimrc
-ln -sf "${HOME}"/dotfiles/.coc-settings.json "${HOME}"/.vim/coc-settings.json
+ln -sf "${HOME}"/dotfiles/coc-settings.json "${HOME}"/.vim/coc-settings.json
