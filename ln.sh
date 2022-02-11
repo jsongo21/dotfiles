4f38fa1da808a026f738646b3e956858341ced44
Jason Ngo <1.ngo.jason@gmail.com>
2022-02-11 15:09:02 +1100

    moved coc-settings to nvim directory

--- ln.sh
+++ ln.sh
@@ -3,4 +3,3 @@ ln -svf "${HOME}"/dotfiles/.zprofile "${HOME}"/.zprofile
 ln -svf "${HOME}"/dotfiles/.tmux.conf "${HOME}"/.tmux.conf
 ln -svf "${HOME}"/dotfiles/.vimrc "${HOME}"/.vimrc
 ln -svf "${HOME}"/dotfiles/.config/nvim "${HOME}"/.config/nvim
-ln -svf "${HOME}"/dotfiles/coc-settings.json "${HOME}"/.vim/coc-settings.json
