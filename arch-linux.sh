17c7d5a6091808ee457f546ecdc7485b88a1f424
Jason Ngo <1.ngo.jason@gmail.com>
2022-08-25 03:58:06 +1000

    feat: add zsh-autosuggestion and bspwm

--- arch-linux.sh
+++ arch-linux.sh
@@ -1,1 +1,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c 'curl -fLo ~/.local/share/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
--- pacman.sh
+++ pacman.sh
@@ -1,6 +1,8 @@
 pacman -Syu
 
-pacman -S --noconfirm zsh zsh-completions neovim
+pacman -S --noconfirm zsh zsh-completions powerline powerline-fonts git neovim
+
 # xorg
 pacman -S --noconfirm xorg-server xorg-apps xorg-xinit 
 
+pacman -S bspwm sxhkd
