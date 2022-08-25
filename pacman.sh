467dca7ae16ad22ce8a04c1e9cb762a4945f5329
Jason Ngo <1.ngo.jason@gmail.com>
2022-08-25 15:46:43 +1000

    feat: add alacritty

--- pacman.sh
+++ pacman.sh
@@ -3,6 +3,6 @@ sudo pacman -Syu
 sudo pacman -S --noconfirm zsh zsh-completions powerline powerline-fonts git neovim
 
 # xorg
-sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit 
+sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit xterm
 
-sudo pacman -S --noconfirm bspwm sxhkd
+sudo pacman -S --noconfirm bspwm sxhkd neofetch alacritty
