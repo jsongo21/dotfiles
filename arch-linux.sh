0838558abb4ea09dde00145c46c261618a44a5d6
Jason Ngo <1.ngo.jason@gmail.com>
2022-08-25 19:47:14 +1000

    fix: polybar launch

--- arch-linux.sh
+++ arch-linux.sh
@@ -3,3 +3,4 @@ sh -c 'curl -fLo ~/.local/share/nvim/autoload/plug.vim --create-dirs https://raw
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts && ./install.sh && cd .. && rm -rf fonts
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
