7c4f1de7db3699f3b84714bc6894228fc15db86b
Jason Ngo <1.ngo.jason@gmail.com>
2022-08-25 14:52:40 +1000

    feat: add sudo to pacman

--- pacman.sh
+++ pacman.sh
@@ -1,8 +1,8 @@
-pacman -Syu
+sudo pacman -Syu
 
-pacman -S --noconfirm zsh zsh-completions powerline powerline-fonts git neovim
+sudo pacman -S --noconfirm zsh zsh-completions powerline powerline-fonts git neovim
 
 # xorg
-pacman -S --noconfirm xorg-server xorg-apps xorg-xinit 
+sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit 
 
-pacman -S bspwm sxhkd
+sudo pacman -S --noconfirm bspwm sxhkd
