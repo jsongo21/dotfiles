90a668d0a652057e80760982f2b7ded92c20be73
Jason Ngo <1.ngo.jason@gmail.com>
2022-08-26 03:13:33 +1000

    fix: polybar config

--- linux/polybar/launch.sh
+++ linux/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-MONITOR=$(polybar -m | tail -1 | sed -e 's/:.*$//g') DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1) polybar bar 
+MONITOR=$(polybar -m | tail -1 | sed -e 's/:.*$//g') DEFAULT_NETWORK_DEVICE=$(ip route | grep '^default' | awk '{print $5}' | head -n1) polybar bar
 
 echo "Polybar launched..."
