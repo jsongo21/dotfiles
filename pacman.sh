1f5c3facbbe449ea4a89dc8323a266d2e52066d0
Jason Ngo <1.ngo.jason@gmail.com>
2022-08-26 22:32:51 +1000

    feat: add noto fonts

--- pacman.sh
+++ pacman.sh
@@ -5,4 +5,4 @@ sudo pacman -S --noconfirm zsh zsh-completions powerline neovim
 # xorg
 sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit xterm
 
-sudo pacman -S --noconfirm bspwm sxhkd neofetch alacritty dmenu polybar pulseaudio
+sudo pacman -S --noconfirm bspwm sxhkd neofetch alacritty dmenu polybar pulseaudio noto-fonts-emoji
