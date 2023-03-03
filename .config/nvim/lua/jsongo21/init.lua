4d530a9df11c0d780ca82168e74e2993246d0971
Jason Ngo <1.ngo.jason@gmail.com>
2023-03-03 16:21:57 +1100

    feat: add yank highlight to nvim

--- .config/nvim/lua/jsongo21/init.lua
+++ .config/nvim/lua/jsongo21/init.lua
@@ -1,2 +1,13 @@
 require("jsongo21.remap")
 require("jsongo21.set")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
