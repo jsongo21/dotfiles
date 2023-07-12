4834bbc7c1ce3f9a6da0943fce9d05e57868fe5b
Jason Ngo <1.ngo.jason@gmail.com>
2023-07-12 18:44:28 +1000

    lint: format file

--- .config/nvim/lua/jsongo21/packer.lua
+++ .config/nvim/lua/jsongo21/packer.lua
@@ -3,9 +3,9 @@
 -- Only required if you have packer configured as `opt`
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -111,6 +111,5 @@ return require('packer').startup(function(use)
   -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
-  end 
-
+  end
 end)
