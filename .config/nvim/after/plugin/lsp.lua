cdf84620d691d14b5a212b209843a6cb4260eacd
Jason Ngo <1.ngo.jason@gmail.com>
2023-07-25 10:52:47 +1000

    feat: update keybind  for autocomplete

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,34 +4,37 @@ lsp.preset("recommended")
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 })
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-f>"] = cmp.mapping.complete(),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
 })
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+  mapping = cmp_mappings,
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, { { name = 'buffer' } })
 })
 
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
 
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -56,14 +59,14 @@ lsp.configure('eslint', {
 lsp.configure('gopls', {
   settings = {
     gopls = {
-      buildFlags = {"-tags=session1 session2 session3 session4 session5 session6"}
+      buildFlags = { "-tags=session1 session2 session3 session4 session5 session6" }
     }
   }
 })
 
 lsp.format_on_save({
   servers = {
-    ['gopls'] = {'go'},
+    ['gopls'] = { 'go' },
   }
 })
 
