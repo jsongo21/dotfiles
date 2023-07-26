28b1450e2e72c619bce7c57f12b50254a4227f91
Jason Ngo <1.ngo.jason@gmail.com>
2023-07-26 16:35:09 +1000

    feat: update autocomplete mapping

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -21,7 +21,7 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ["<C-f>"] = cmp.mapping.complete(),
+  ["<C-Space>"] = cmp.mapping.complete(),
   ['<CR>'] = cmp.mapping.confirm({ select = true }),
 })
 
