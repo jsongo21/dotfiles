9fd6c0095144f888a65184db1cf30801d7839aef
Jason Ngo <1.ngo.jason@gmail.com>
2023-08-25 12:20:56 +1000

    feat: add ripgrep args

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>gs', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gs', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
--- .config/nvim/lua/jsongo21/packer.lua
+++ .config/nvim/lua/jsongo21/packer.lua
@@ -20,7 +20,13 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { { 'nvim-lua/plenary.nvim' } }
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end
   }
 
   ---- GRUVBOX ----
