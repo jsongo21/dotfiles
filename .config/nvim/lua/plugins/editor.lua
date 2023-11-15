f48bb18dcf5e07e02b36d390ebba2d05930f7c11
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2023-11-15 16:09:17 +1100

    feat: add autopairs

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,64 +1,90 @@
 return {
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
 
-  -- Time tracking
-  'wakatime/vim-wakatime',
+    -- Time tracking
+    'wakatime/vim-wakatime',
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-  {
-    'echasnovski/mini.indentscope',
-    name = 'mini.indentscope',
-    event = 'VeryLazy',
-    config = function() 
-      require('mini.indentscope').setup({
-          draw = {
-              delay = 40,
-              priority = 20,
-              animation = require('mini.indentscope').gen_animation.exponential({
-                  easing = 'in-out',
-                  duration = 80,
-                  unit = 'total',
-              }),
-          },
-          symbol = '┃',
-          options = {
-              try_as_border = true,
-          },
-          mappings = {
-              goto_top = '',
-              goto_bottom = '',
-              object_scope = '',
-              object_scope_with_border = '',
-          },
-    })
+    {
+        -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = 'ibl',
+        opts = {},
+    },
+    {
+        'echasnovski/mini.indentscope',
+        name = 'mini.indentscope',
+        event = 'VeryLazy',
+        config = function()
+            require('mini.indentscope').setup({
+                draw = {
+                    delay = 40,
+                    priority = 20,
+                    animation = require('mini.indentscope').gen_animation.exponential({
+                        easing = 'in-out',
+                        duration = 80,
+                        unit = 'total',
+                    }),
+                },
+                symbol = '┃',
+                options = {
+                    try_as_border = true,
+                },
+                mappings = {
+                    goto_top = '',
+                    goto_bottom = '',
+                    object_scope = '',
+                    object_scope_with_border = '',
+                },
+            })
 
-    local disabled = {
-        'harpoon',
-        'help',
-        'terminal',
-    }
+            local disabled = {
+                'harpoon',
+                'help',
+                'terminal',
+            }
 
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = '*',
-      callback = function()
-        if disabled[vim.bo.filetype] ~= nil or vim.bo.buftype ~= '' then
-          vim.b.miniindentscope_disable = true
-        end
-      end,
-    })
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = '*',
+                callback = function()
+                    if disabled[vim.bo.filetype] ~= nil or vim.bo.buftype ~= '' then
+                        vim.b.miniindentscope_disable = true
+                    end
+                end,
+            })
 
-    vim.cmd([[highlight! link MiniIndentscopeSymbol Identifier]])
-  end
-  },
+            vim.cmd([[highlight! link MiniIndentscopeSymbol Identifier]])
+        end,
+    },
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+    -- Auto Pairs
+    {
+        'windwp/nvim-autopairs',
+        config = function() require('nvim-autopairs').setup({}) end,
+    },
+    {
+        'windwp/nvim-ts-autotag',
+        config = function() require('nvim-ts-autotag').setup({}) end,
+    },
+
+    -- "gc" to comment visual regions/lines
+    {
+        'numToStr/Comment.nvim',
+        dependencies = {
+            'JoosepAlviste/nvim-ts-context-commentstring',
+        },
+        config = function()
+            require('Comment').setup({
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+            })
+        end,
+    },
+
+    -- Vim Surround
+    {
+        'kylechui/nvim-surround',
+        config = function() require('nvim-surround').setup({}) end,
+    },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,101 +2,6 @@ return {
     -- NOTE: This is where your plugins related to LSP can be installed.
     --  The configuration is done below. Search for lspconfig to find it below.
     {
-        -- LSP Configuration & Plugins
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-
-            -- Useful status updates for LSP
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            { 'j-hui/fidget.nvim', opts = {} },
-
-            -- Additional lua configuration, makes nvim stuff amazing!
-            'folke/neodev.nvim',
-        },
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' },
-                        },
-                    },
-                },
-            })
-            lspconfig.pyright.setup({})
-            lspconfig.rust_analyzer.setup({
-                -- Server-specific settings. See `:help lspconfig-setup`
-                settings = {
-                    ['rust-analyzer'] = {},
-                },
-            })
-            lspconfig.tsserver.setup({})
-
-            -- Global mappings.
-            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-            -- Use LspAttach autocommand to only map the following keys
-            -- after the language server attaches to the current buffer
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                callback = function(ev)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-                    -- Buffer local mappings.
-                    -- See `:help vim.lsp.*` for documentation on any of the below functions
-                    local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                    vim.keymap.set(
-                        'n',
-                        '<space>wl',
-                        function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-                        opts
-                    )
-                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                    vim.keymap.set(
-                        'n',
-                        '<space>f',
-                        function() vim.lsp.buf.format({ async = true }) end,
-                        opts
-                    )
-                end,
-            })
-        end,
-    },
-    {
-        -- Autocompletion
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-
-            -- Adds LSP completion capabilities
-            'hrsh7th/cmp-nvim-lsp',
-
-            -- Adds a number of user-friendly snippets
-            'rafamadriz/friendly-snippets',
-        },
-    },
-    {
         'williamboman/mason.nvim',
         cmd = 'Mason',
         keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
@@ -107,7 +12,6 @@ return {
                 'shfmt',
             },
         },
-        ---@param opts MasonSettings | {ensure_installed: string[]}
         config = function(_, opts)
             require('mason').setup(opts)
             local mr = require('mason-registry')
@@ -148,8 +52,145 @@ return {
                     'vimls',
                     'yamlls',
                 },
+                automatic_installation = true,
             })
         end,
+    },
+    {
+        -- LSP Configuration & Plugins
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            -- Automatically install LSPs to stdpath for neovim
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim', opts = {} },
+
+            -- Additional lua configuration, makes nvim stuff amazing!
+            'folke/neodev.nvim',
+        },
+        config = function()
+            local lspconfig = require('lspconfig')
+
+            local lsp_defaults = lspconfig.util.default_config
+            lsp_defaults.capabilities =
+                require('cmp_nvim_lsp').default_capabilities(lsp_defaults.capabilities) -- additional capabilities from completions
+
+            lspconfig.eslint.setup({
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        buffer = bufnr,
+                        command = 'EslintFixAll',
+                    })
+                end,
+            })
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
+                    },
+                },
+            })
+            lspconfig.pyright.setup({})
+            lspconfig.rust_analyzer.setup({
+                -- Server-specific settings. See `:help lspconfig-setup`
+                settings = {
+                    ['rust-analyzer'] = {},
+                },
+            })
+            lspconfig.tailwindcss.setup({})
+            lspconfig.tsserver.setup({})
+
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+
+                    -- In this case, we create a function that lets us more easily define mappings specific
+                    -- for LSP related items. It sets the mode, buffer and description for us each time.
+                    local nmap = function(keys, func, desc)
+                        if desc then desc = 'LSP: ' .. desc end
+
+                        vim.keymap.set(
+                            'n',
+                            keys,
+                            func,
+                            { buffer = ev.buf, desc = desc, remap = false }
+                        )
+                    end
+
+                    -- Go to definitions and references
+                    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+                    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                    nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+                    nmap('gt', vim.lsp.buf.type_definition, '[G]oto [T]ype Definition')
+                    nmap('K', vim.lsp.buf.hover, 'Hover definition')
+                    nmap(
+                        '<leader>ws',
+                        require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                        '[W]orkspace [S]ymbol'
+                    )
+                    nmap(
+                        '<leader>ds',
+                        require('telescope.builtin').lsp_document_symbols,
+                        '[D]ocument [S]ymbol'
+                    )
+                    vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+
+                    -- Errors inspection
+                    nmap('<leader>vd', vim.diagnostic.open_float, 'Open [F]loat')
+                    nmap('[d', vim.diagnostic.goto_next, 'Goto [N]ext')
+                    nmap(']d', vim.diagnostic.goto_prev, 'Goto [P]rev')
+                    nmap('<leader>vl', '<cmd>Telescope diagnostics<cr>', '[L]ist')
+
+                    -- Restart server
+                    nmap('<leader>rs', '<cmd>LspRestart<CR>', '[R]estart [S]erver')
+                    -- Formatting
+                    nmap('<leader>fm', function()
+                        vim.lsp.buf.format({ async = true })
+                        print('formatted')
+                    end, '[F]ormat')
+                end,
+            })
+        end,
+    },
+    {
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+
+            -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+
+            -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets',
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        },
     },
     {
         'stevearc/conform.nvim',
