1a9286408ac93d5e9f9739eba5850f765ff2e1a8
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2023-12-09 03:06:46 +1100

    feat: use harpoon2 branch

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,11 +1,12 @@
 return {
     'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
     config = function()
         local harpoon = require('harpoon')
         ---@diagnostic disable-next-line: missing-parameter
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
+        vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     end,
 }
