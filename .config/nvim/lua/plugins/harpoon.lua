ea70aa7546c1cbe9b4d6780674cb6a5abe9ea478
Jason Ngo <1.ngo.jason@gmail.com>
2023-12-11 16:09:53 +1100

    feat: add harpoon next/prev binds

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -8,5 +8,7 @@ return {
 
         vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
         vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<C-t>", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
     end,
 }
