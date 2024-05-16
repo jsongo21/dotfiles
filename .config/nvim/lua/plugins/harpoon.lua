9ea194a29796259e5abd194520b27f5cc6345a77
Jason Ngo <1.ngo.jason@gmail.com>
2024-05-16 21:04:45 +1000

    feat: use telescope ui for harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -11,8 +11,25 @@ return {
             }
         })
 
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder       = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer    = conf.file_previewer({}),
+                sorter       = conf.generic_sorter({})
+            }):find()
+        end
+
         vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
-        vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
         vim.keymap.set("n", "<C-t>", function() harpoon:list():prev() end)
         vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
     end,
