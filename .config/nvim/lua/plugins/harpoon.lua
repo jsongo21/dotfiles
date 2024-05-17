23410e0c6d85f18f097a06a4199c53c1e04e9811
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2024-05-17 18:33:42 +1000

    feat: change to append harpoon add

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,13 +7,13 @@ return {
         harpoon:setup({
             settings = {
                 sync_on_ui_close = true,
-                save_on_toggle = true
-            }
+                save_on_toggle = true,
+            },
         })
 
-        vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
+        vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
         vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+        vim.keymap.set('n', '<C-t>', function() harpoon:list():prev() end)
+        vim.keymap.set('n', '<C-n>', function() harpoon:list():next() end)
     end,
 }
