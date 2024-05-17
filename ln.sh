729de7bea93e597a02ac3801d4b2d8ecc6a56746
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2024-05-17 18:59:29 +1000

    feat: update link script

--- ln.sh
+++ ln.sh
@@ -2,7 +2,7 @@ ln -svf "${HOME}"/dotfiles/.zshrc "${HOME}"/.zshrc
 ln -svf "${HOME}"/dotfiles/.zprofile "${HOME}"/.zprofile
 ln -svf "${HOME}"/dotfiles/.tmux.conf "${HOME}"/.tmux.conf
 mkdir -p "${HOME}"/.config && ln -svf "${HOME}"/dotfiles/.config/nvim "${HOME}"/.config/nvim
-ln -svf "${HOME}"/dotfiles//.alacritty.yml "${HOME}"/.alacritty.yml
+ln -svf "${HOME}"/dotfiles//.alacritty.toml "${HOME}"/.alacritty.toml
 
 unamestr=$(uname)
 if [ "$unamestr" = "Linux" ]; then
