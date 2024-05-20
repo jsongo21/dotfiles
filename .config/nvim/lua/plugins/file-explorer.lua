42d3572a97fc4eccbecc1f69d46ab2295609b2a8
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2024-05-20 13:18:41 +1000

    feat: add oil

---
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -0,0 +1,14 @@
+return {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('oil').setup({
+            view_options = {
+                show_hidden = true,
+            },
+        })
+
+        vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+}
