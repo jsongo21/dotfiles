46db459e898a2332859db2df6577bfbaca7052f1
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2024-05-22 16:08:54 +1000

    feat: add remap for oil

--- .config/nvim/lua/plugins/file-explorer.lua
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -10,6 +10,7 @@ return {
         })
 
         vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+        vim.keymap.set('n', '<leader>pv', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
         vim.o.splitright = true
     end,
 }
