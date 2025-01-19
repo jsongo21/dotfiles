21f39417a4bce39aa6ca84e7eb9ed1f1e171360e
Jason Ngo <1.ngo.jason@gmail.com>
2025-01-19 16:03:55 +1100

    remove lspkind

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -205,7 +205,7 @@ return {
         -- optional: provides snippets for the snippet source
         dependencies = {
             'rafamadriz/friendly-snippets',
-            'onsails/lspkind.nvim',
+            'giuxtaposition/blink-cmp-copilot',
         },
 
         -- use a release tag to download pre-built binaries
@@ -256,16 +256,6 @@ return {
                             { 'label', 'label_description', gap = 1 },
                             { 'kind_icon', 'kind' },
                         },
-                        components = {
-                            kind_icon = {
-                                ellipsis = false,
-                                text = function(ctx)
-                                    return require('lspkind').symbolic(ctx.kind, {
-                                        mode = 'symbol',
-                                    })
-                                end,
-                            },
-                        },
                         treesitter = {
                             'lsp',
                         },
@@ -281,6 +271,39 @@ return {
                 -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
                 -- Adjusts spacing to ensure icons are aligned
                 nerd_font_variant = 'mono',
+                kind_icons = {
+                    Copilot = '',
+                    Text = '󰉿',
+                    Method = '󰊕',
+                    Function = '󰊕',
+                    Constructor = '󰒓',
+
+                    Field = '󰜢',
+                    Variable = '󰆦',
+                    Property = '󰖷',
+
+                    Class = '󱡠',
+                    Interface = '󱡠',
+                    Struct = '󱡠',
+                    Module = '󰅩',
+
+                    Unit = '󰪚',
+                    Value = '󰦨',
+                    Enum = '󰦨',
+                    EnumMember = '󰦨',
+
+                    Keyword = '󰻾',
+                    Constant = '󰏿',
+
+                    Snippet = '󱄽',
+                    Color = '󰏘',
+                    File = '󰈔',
+                    Reference = '󰬲',
+                    Folder = '󰉋',
+                    Event = '󱐋',
+                    Operator = '󰪚',
+                    TypeParameter = '󰬛',
+                },
             },
 
             signature = {
@@ -290,11 +313,26 @@ return {
             -- Default list of enabled providers defined so that you can extend it
             -- elsewhere in your config, without redefining it, due to `opts_extend`
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                default = { 'copilot', 'lsp', 'path', 'snippets', 'buffer' },
                 cmdline = {},
                 providers = {
                     lsp = {
                         score_offset = 0, -- Boost/penalize the score of the items
+                    },
+                    copilot = {
+                        name = 'copilot',
+                        module = 'blink-cmp-copilot',
+                        score_offset = 100,
+                        async = true,
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = 'Copilot'
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
                     },
                 },
             },
