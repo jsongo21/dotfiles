680a39f0f216f4432a470d1ae4e101cc1fbe591a
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2025-02-13 10:35:38 +1100

    feat: move automatic tmux installation to tmuxconf

--- install/macos.sh
+++ install/macos.sh
@@ -1,15 +1,19 @@
+#!/usr/bin/env bash
+
+# Get the directory of the script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BREWFILE_DIR="$SCRIPT_DIR/../mac/Brewfile"
+
 xode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval $(/opt/homebrew/bin/brew shellenv)
 sudo chmod -R 755 /opt/homebrew/share
-brew bundle
+brew bundle --file=$BREWFILE_DIR
 
 # Setup Terminal
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/geometry-zsh/geometry ${HOME}/.oh-my-zsh/themes/geometry
 sudo xattr -r -d com.apple.quarantine /Applications/Alacritty.app/
 sudo xattr -r -d com.apple.quarantine /opt/homebrew/bin/chromedriver
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
