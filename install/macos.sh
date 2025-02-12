6efb345c9c21c152bb72c06a264353df22ce90ed
Jason Ngo <29261202+jsongo21@users.noreply.github.com>
2025-02-12 19:54:06 +1100

    feat: update brew file

--- install/macos.sh
+++ install/macos.sh
@@ -4,68 +4,12 @@ xode-select --install
 
 eval $(/opt/homebrew/bin/brew shellenv)
 sudo chmod -R 755 /opt/homebrew/share
-brew update
+brew bundle
 
 # Setup Terminal
-brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/geometry-zsh/geometry ${HOME}/.oh-my-zsh/themes/geometry
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-curl -o- -L https://yarnpkg.com/install.sh | bash
-
-
-# Setup Terminal Install apps
-brew install neovim
-brew install luarocks
-brew install cmake
-brew install tmux
-brew install go
-brew install golangci-lint
-brew install ripgrep
-brew install --cask google-chrome
-brew install --cask brave-browser
-brew install --cask iterm2
-brew install --cask visual-studio-code
-brew install --cask rectangle
-brew install --cask slack
-brew install --cask obsidian
-brew install --cask scroll-reverser
-brew install --cask docker
-brew install --cask zoom
-brew install --cask spotify
-brew install --cask postman
-brew install --cask discord
-brew install --cask chromedriver
-brew install --cask adobe-acrobat-reader
-brew install docker-compose
-brew install docker-credential-helper
-brew install awscli
-brew install pipx
-brew install direnv
-brew install awslogs
-brew install jq
-brew install alacritty
 sudo xattr -r -d com.apple.quarantine /Applications/Alacritty.app/
 sudo xattr -r -d com.apple.quarantine /opt/homebrew/bin/chromedriver
-brew install --cask font-jetbrains-mono-nerd-font
-brew install mkcert
-brew tap hashicorp/tap
-brew install hashicorp/tap/terraform
-brew install coreutils
-brew install graphviz
-brew tap microsoft/git
-brew install --cask git-credential-manager-core
-brew install asdf
-brew install --cask nikitabobko/tap/aerospace
-
-curl -s "https://get.sdkman.io" | bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# install aws-sso-util
-pipx ensurepath
-pipx install aws-sso-util
 
