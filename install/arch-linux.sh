bfe13301732fe12f75ce46b93e0318ecd76e9a27
Jason Ngo <1.ngo.jason@gmail.com>
2025-02-24 18:40:55 +1100

    feat: update arch linux install script

--- install/arch-linux.sh
+++ install/arch-linux.sh
@@ -1,10 +1,2 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts && ./install.sh && cd .. && rm -rf fonts
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
-yay -S --noconfirm nvm ttf-material-design-icons nerd-fonts-jetbrains-mono
-source /usr/share/nvm/init-nvm.sh && nvm install --lts
-nvim +PlugInstall +qall
-
+pacman -S --noconfirm - < pacman
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
