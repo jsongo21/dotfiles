b43fb37f9d2e12d5e6688b4f598f4c09f043614b
Jason Ngo <1.ngo.jason@gmail.com>
2025-02-24 20:52:35 +1100

    feat add deno

--- install/arch-linux.sh
+++ install/arch-linux.sh
@@ -1,2 +1,2 @@
 pacman -S --noconfirm - < pacman
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
