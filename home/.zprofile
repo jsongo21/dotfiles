1f71bee387cdf6b1d310e7139ac107263f8173be
Jason Ngo <1.ngo.jason@gmail.com>
2025-02-24 22:02:05 +1100

    feat: update pacman

--- home/.zprofile
+++ home/.zprofile
@@ -2,6 +2,3 @@ setopt ignoreeof
 
 # Created by `pipx` on 2022-11-16 03:29:27
 export PATH="$PATH:/Users/jason/.local/bin"
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-
-eval "$(/opt/homebrew/bin/brew shellenv)"
--- install/arch-linux.sh
+++ install/arch-linux.sh
@@ -1,2 +1,3 @@
-pacman -S --noconfirm - < pacman
+sudo pacman -S --noconfirm - < pacman
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
